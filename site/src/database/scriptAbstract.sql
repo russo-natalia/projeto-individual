014ba98e3db5589de2069be6c70e61a46f06e897
russo-natalia <natalia.russo@sptech.school>
2023-05-22 18:19:30 -0300

    atualização do bd

--- site/src/database/scriptAbstract.sql
+++ site/src/database/scriptAbstract.sql
@@ -17,11 +17,25 @@ idResposta int primary key auto_increment,
 fkUsuario int,
 constraint fkUsuarioResposta foreign key (fkUsuario) references usuario(idUsuario),
 resultado varchar(100),
-dtResposta DATETIME default current_timestamp
+dtHorario DATETIME default current_timestamp,
+questao1 varchar(45),
+questao2 varchar(45),
+questao3 varchar(45),
+questao4 varchar(45),
+questao5 varchar(45)
 );
+-- ver a questão do relacionamento forte/ pk composta
 
 create table faleConosco (
 idContato int primary key auto_increment,
 mensagem varchar(250),
 emailContato varchar(50)
 )auto_increment = 100;
+
+delete from usuario where idUsuario = 8;
+
+desc respostas;
+select * from usuario;
+select * from faleConosco;
+select * from respostas;
+
