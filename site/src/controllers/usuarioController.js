40c58d9a9a6192a4696fd96ca9af3e66ecdefca0
russo-natalia <natalia.russo@sptech.school>
2023-05-22 14:51:31 -0300

    Fale conosco funcionando

--- site/src/controllers/usuarioController.js
+++ site/src/controllers/usuarioController.js
@@ -97,9 +97,41 @@ function cadastrar(req, res) {
     }
 }
 
+function cadastrarFale(req, res) {
+    // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
+    var emailFale = req.body.emailFaleServer;
+    var mensagemFale = req.body.mensagemFaleServer;
+
+    // Faça as validações dos valores
+    if (emailFale == undefined) {
+        res.status(400).send("Seu email está undefined!");
+    } else if (mensagemFale == undefined) {
+        res.status(400).send("Seu mensagem está undefined!");
+    } else {
+        
+        // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
+        usuarioModel.cadastrarFale(emailFale, mensagemFale)
+            .then(
+                function (resultado) {
+                    res.json(resultado);
+                }
+            ).catch(
+                function (erro) {
+                    console.log(erro);
+                    console.log(
+                        "\nHouve um erro ao realizar o cadastro! Erro: ",
+                        erro.sqlMessage
+                    );
+                    res.status(500).json(erro.sqlMessage);
+                }
+            );
+    }
+}
+
 module.exports = {
     entrar,
     cadastrar,
     listar,
-    testar
+    testar,
+    cadastrarFale,
 }
--- site/src/routes/usuarios.js
+++ site/src/routes/usuarios.js
@@ -16,6 +16,10 @@ router.post("/cadastrar", function (req, res) {
     usuarioController.cadastrar(req, res);
 })
 
+router.post("/cadastrarFale", function (req, res) {
+    usuarioController.cadastrarFale(req, res);
+})
+
 router.post("/autenticar", function (req, res) {
     usuarioController.entrar(req, res);
 });
