45ae0871977912444d408db1c29ffd29407e0c00
russo-natalia <natalia.russo@sptech.school>
2023-05-31 20:35:54 -0300

    selects

--- site/src/database/scriptAbstract.sql
+++ site/src/database/scriptAbstract.sql
@@ -1,5 +1,4 @@
 create database abstract;
-
 use abstract;
 
 create table usuario (
@@ -7,7 +6,7 @@ idUsuario int primary key auto_increment,
 nome varchar(45),
 sobrenome varchar(100),
 dtNasc date,
-telefone varchar(14),
+telefone varchar(11),
 email varchar(50),
 senha varchar(20)
 );
@@ -17,6 +16,7 @@ idResposta int primary key auto_increment,
 fkUsuario int,
 constraint fkUsuarioResposta foreign key (fkUsuario) references usuario(idUsuario),
 resultado varchar(100),
+artista varchar(100),
 dtHorario DATETIME default current_timestamp,
 questao1 varchar(45),
 questao2 varchar(45),
@@ -32,10 +32,24 @@ mensagem varchar(250),
 emailContato varchar(50)
 )auto_increment = 100;
 
-delete from usuario where idUsuario = 8;
 
 desc respostas;
 select * from usuario;
 select * from faleConosco;
 select * from respostas;
 
+ -- Resultados x Tentativas de usuario
+select resultado, count(resultado) from respostas where fkUsuario = 1 group by resultado;
+-- tentativas x meses de usuario
+select date_format(dtHorario, '%m') as mes, count(*) as tentativas from respostas where fkUsuario = 1 group by date_format(dtHorario, '%m');
+
+ -- Resultados x Tentativas
+select resultado, count(resultado) from respostas group by resultado;
+-- tentativas x meses
+select date_format(dtHorario, '%m') as mes, count(*) as tentativas from respostas group by date_format(dtHorario, '%m');
+-- relação de artistas
+select artista, count(resultado) from respostas group by artista;
+-- relação de caracteristicas
+select questao3, count(resultado) from respostas group by questao3;
+-- relação de hobbies 
+select questao4, count(resultado) from respostas group by questao4;
--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -5,17 +5,9 @@ function buscarUltimasMedidas(idResposta, limite_linhas) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-                        idResposta,
-                        dtHorario as momento_grafico
-                    from respostas
-                    order by idResposta desc`;
+        instrucaoSql = `select resultado, count(resultado) from respostas group by resultado`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        idResposta,
-                      dtHorario as momento_grafico
-                    from respostas
-                    order by idResposta desc limit ${limite_linhas}`;
+        instrucaoSql = `select resultado, count(resultado) from respostas group by resultado`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -30,18 +22,10 @@ function buscarMedidasEmTempoReal(idResposta) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        idResposta,
-        dtHorario as momento_grafico
-                        from respostas
-                    order by idResposta desc`;
+        instrucaoSql = `select resultado, count(resultado) as nome from respostas group by resultado`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        idResposta,
-        dtHorario as momento_grafico 
-                        from respostas
-                    order by idResposta desc limit 1`;
+        instrucaoSql = `select resultado, count(resultado) from respostas group by resultado`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
