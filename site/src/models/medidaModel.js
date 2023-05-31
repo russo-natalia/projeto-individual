b2a754d8815dcc64385f568bc8064cca66d2cf07
russo-natalia <natalia.russo@sptech.school>
2023-05-31 20:47:48 -0300

    GRAFICO FUNCIONANDOOOOOOOOOOOOOOOOOOOOO AAAAAAAAAAAAAAAAAAAAAAAAA

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -5,9 +5,9 @@ function buscarUltimasMedidas(idResposta, limite_linhas) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select resultado, count(resultado) from respostas group by resultado`;
+        instrucaoSql = `select resultado, count(resultado) as num from respostas group by resultado`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select resultado, count(resultado) from respostas group by resultado`;
+        instrucaoSql = `select resultado, count(resultado) as num from respostas group by resultado`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -22,10 +22,10 @@ function buscarMedidasEmTempoReal(idResposta) {
     instrucaoSql = ''
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select resultado, count(resultado) as nome from respostas group by resultado`;
+        instrucaoSql = `select resultado, count(resultado) as num from respostas group by resultado`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select resultado, count(resultado) from respostas group by resultado`;
+        instrucaoSql = `select resultado, count(resultado) as num from respostas group by resultado`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
